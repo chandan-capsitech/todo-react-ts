09f6a1c463543aa72f36f9133e1e3d611f6133c8
chandan-capsitech <chandan.parida@capsitech.com>
2025-06-23 18:54:53 +0530

    change the header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ type HeaderProps = {
 
 const Header = ({ title }: HeaderProps) => {
   return (
-    <h1 className="text-3xl font-bold text-center my-4 text-indigo-800">{title}</h1>
+    <h1 className="text-3xl font-bold text-center my-4 text-yellow-400">{title}</h1>
   );
 }
 
